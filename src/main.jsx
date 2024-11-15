fb2ae3c0c362c817949cf090baa090d2e1d610eb
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-15 15:00:35 +0530

    make other routed as children router of Root element

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
@@ -15,42 +12,42 @@ import Plays from "./routes/plays";
 import Activities from "./routes/activities";
 import Sports from "./routes/sports";
 
-
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
+    element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+      {
+        path: "/movies/:movieId", // :Id -> request / Url parameter
+        element: <Movie />,
+      },
+      {
+        path: "/stream",
+        element: <Stream />,
+      },
+      {
+        path: "/events",
+        element: <Events />,
+      },
+      {
+        path: "/plays",
+        element: <Plays />,
+      },
+      {
+        path: "/sports",
+        element: <Sports />,
+      },
+      {
+        path: "/activities",
+        element: <Activities />,
+      },
+    ],
   },
-  {
-    path: "/movies",
-    element: <Movies/>,
-  },
-  {
-    path: "/movies/:movieId", // :Id -> request / Url parameter
-    element: <Movie/>,
-  },
-  {
-    path: "/stream",
-    element: <Stream/>,
-  },
-  {
-    path: "/events",
-    element: <Events/>,
-  },
-  {
-    path: "/plays",
-    element: <Plays/>,
-  },
-  {
-    path: "/sports",
-    element: <Sports/>,
-  },
-  {
-    path: "/activities",
-    element: <Activities/>,
-  },
-
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../components/header'
+import { Outlet } from 'react-router-dom'
 
 export default function Root(props) {
   return (
     <div>
       <Header/>
-      <main>main</main>
+      <Outlet/>
       <footer>footer</footer>
     </div>
   )
