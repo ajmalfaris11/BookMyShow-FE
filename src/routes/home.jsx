1e25939ca9ee67f3977e9a5976e81fbeb0c31562
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-22 04:37:16 +0530

    Added Recomended Movie section one more time just for view purpose it will remove after add other sections

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -36,6 +36,15 @@ export default function Home() {
      <div className="w-full py-20 px-10">
           <img src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/stream-leadin-web-collection-202210241242.png" alt="" />
      </div>
+
+     <div className="p-4">
+        <h2 className="text-2xl font-bold mb-4">Recommended Movies</h2>
+        <div className="flex flex-wrap gap-8 justify-center">
+          {movies.map((movie) => (
+            <MovieCard key={movie._id} movie={movie} />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
