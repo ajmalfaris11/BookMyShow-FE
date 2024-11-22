238891af2ff12f0f150e12f640ed7aed0898c666
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-22 20:40:41 +0530

    MovieCard Component imported to Movies page

--- src/routes/movies.jsx
+++ src/routes/movies.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/movieCard";
+
+
+
 
 export default function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  // Fetch movie data from the backend
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/movies"); // API endpoint
+        const data = await response.json();
+        setMovies(data); // Assuming the backend sends movies under "movies"
+      } catch (error) {
+        console.error("Failed to fetch movies:", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      movie text designed in tailwind css
-    </h1>
-  )
+    <div className="flex p-10 bg-slate-100">
+      {/* ====== FILTER SECTION ====== */}
+      
+      <div>
+        {/* ====== Movies In Location ====== */}
+        <div>
+          <h2>Location based </h2>
+        </div>
+
+        {/* ===== Movies Section ====== */}
+        <div className="p-4">
+          <div className="flex flex-wrap gap-8 justify-center">
+            {movies.map((movie) => {
+              return <MovieCard key={movie._id} movie={movie} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
